b4091f7767ccb28492114e553732961c6b0a5ba1
NikDoe <nikd0e@yandex.ru>
2023-05-27 11:05:21 +0300

    feat: get posts from fake api

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,30 @@
-export default function Posts() {
-  return <h1>список постов</h1>;
+import Link from "next/link";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+async function getPosts(): Promise<Post[]> {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json();
+}
+
+export default async function Posts() {
+  const posts = await getPosts();
+  
+  return (
+    <>
+      <h1>список постов</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
