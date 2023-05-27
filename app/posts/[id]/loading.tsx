1883315773c79b9efdf2083c8cf71d40195b75e6
NikDoe <nikd0e@yandex.ru>
2023-05-27 11:28:26 +0300

    feat: loading spinner components

---
+++ app/posts/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function SinglePostSpinner() {
+  return <h1>Загружаем статью...</h1>;
+}
---
+++ app/posts/loading.tsx
@@ -0,0 +1,3 @@
+export default function PostsSpinner() {
+  return <h1>Загружаем посты...</h1>;
+}
