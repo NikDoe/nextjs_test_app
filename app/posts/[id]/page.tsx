021def54b2074ada334766d35ed4e5e4210a96ff
NikDoe <nikd0e@yandex.ru>
2023-05-27 11:17:22 +0300

    feat: get single post & refactor metadata

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -6,13 +6,35 @@ type Props = {
   };
 };
 
-export async function generateMetadata({ params: { id } }: Props): Promise<Metadata> {
-	return {
-		title: `страница поста ${id}`,
-	}
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  const { title } = await getSinglePost(id);
+  return {
+    title,
+  };
 }
 
-export default function Post({ params: { id } }: Props) {
-  return <h1>пост {id} </h1>;
+async function getSinglePost(id: string): Promise<Post> {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  return response.json();
 }
 
+export default async function Post({ params: { id } }: Props) {
+  const post = await getSinglePost(id);
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
+}
